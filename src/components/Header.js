3100634013f6d5faec859f4d577504f444558580
Abhi97shek <abhi.kumar736@gmail.com>
2021-10-01 15:12:02 +0530

    Header Component Completed

---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled, { StyledComponent } from 'styled-components';
+import "./Header.css";
+
+
+const Header= ()=>{
+    return(
+        <nav class="navbar">
+            <div class="logo">
+                    <img src="/images/logo.svg" class="logo-disney"></img> 
+            </div>  
+            <div class="menu-list">
+                    <a>
+                        <img src="images/home-icon.svg"></img>
+                        <span>HOME</span>
+                    </a>
+                    <a>
+                        <img src="images/search-icon.svg"></img>
+                        <span>SEARCH</span>
+                    </a>
+                    <a>
+                        <img src="images/watchlist-icon.svg"></img>
+                        <span>WATCHLIST</span>
+                    </a>
+                    <a>
+                        <img src="images/original-icon.svg"></img>
+                        <span>ORIGINAL</span>
+                    </a>
+                    <a>
+                        <img src="images/movie-icon.svg"></img>
+                        <span>MOVIES</span>
+                    </a>
+                    <a>
+                        <img src="images/series-icon.svg"></img>
+                        <span>SERIES</span>
+                    </a>
+        
+            </div>        
+
+
+            <div class="profile">
+                   <img src="images/profile.png"></img>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            Home components
+        </div>
+    )
+}
+
+export default Home
